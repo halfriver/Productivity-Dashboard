b7163de94685679e7e47fec505da2b4b7f9561dd
halfriver <ohkawa2.0@gmail.com>
2020-08-18 18:58:58 -0400

    implement basic drag/drop, nav bar

---
+++ static/js/productivity.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function() {
+  var scriptInterpreter;
+  $(function() {
+    $(".column" ).sortable({
+      connectWith: ".column",
+      handle: ".card-header",
+      cancel: ".card-toggle",
+      placeholder: "card-placeholder ui-corner-all"
+    });
+
+    $(".card" )
+      .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+      .find( ".card-header" )
+        .addClass( "ui-widget-header ui-corner-all" )
+        .prepend( "<span class='ui-icon ui-icon-minusthick card-toggle'></span>");
+
+        $( ".card-toggle" ).on( "click", function() {
+        var icon = $( this );
+        icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
+        icon.closest( ".card" ).find( ".card-content" ).toggle();
+      });
+  });
+});
